95c2a49274ef21453272d97fbc17d184ff6cf906
SyedTDK <syedfaizullah123@gmail.com>
2024-01-31 23:27:32 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-              <h2 className="mt-10 text-center text-4xl font-bold leading-9 tracking-tight text-white">HealthME</h2>
-              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Sign in to your account</h2>
-            </div>
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 className="mt-10 text-center text-4xl font-bold leading-9 tracking-tight text-white">HealthME</h2>
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Sign in to your account</h2>
+          </div>
 
           // Interface for user to sign in
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
