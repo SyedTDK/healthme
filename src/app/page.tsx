af16a865625fbebd6abeee59b75655e0fe3aa2fc
SyedTDK <syedfaizullah123@gmail.com>
2024-03-04 00:55:54 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 //Landing page for the application
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 className="mt-10 text-center text-4xl font-bold leading-9 tracking-tight text-white">HealthME</h2>
+            <img src="/logo.png" alt="logo" className="mx-auto w-24" />
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Coming Soon!</h2>
           </div>
       </div>
