f7852ede0415aa57874fe3a3350f41ea0e99b04e
SyedTDK <syedfaizullah123@gmail.com>
2024-04-04 21:20:12 -0400

    new landing page, chat page

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,96 +1,106 @@
-// Chat UI
-"use client";
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { UserCircleIcon } from '@heroicons/react/16/solid';
-import TextareaAutosize from 'react-textarea-autosize';
-import SideBar from '../components/SideBar';
-import { useSession } from "next-auth/react"; // Import useSession hook
-
-const App: React.FC = () => {
-  // Initialize State for Messages
-  const [messages, setMessages] = useState<string[]>([]);
-  const [newMessage, setNewMessage] = useState('');
-  const { data: session, status } = useSession(); // Use useSession hook
-
-  useEffect(() => {
-    // Fetch Messages from Backend
-    fetchMessages();
-  }, []);
-
-  const fetchMessages = async () => {
-    try {
-      // Make a GET request to fetch messages from the backend
-      const response = await axios.get('https://healthme-test-1868220c15ef.herokuapp.com/chatbot');
-      const chatbotMessages = response.data.messages.map((message: string) => ({ text: message, isUser: false }));
-      setMessages(chatbotMessages);
-    } catch (error) {
-      console.error('Error fetching messages:', error);
-    }
-  };
+import Link from "next/link";
+import Profile from "../components/Profile";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/libs/auth";
+import prisma from "@/app/libs/prisma";
 
 
-  const sendMessage = async () => {
-    if (!newMessage.trim()) return;
-
-    try {
-      // Make a POST request to send the user's message to the chatbot API
-      const response = await axios.post('https://healthme-test-1868220c15ef.herokuapp.com/chatbot', { message: newMessage });
-
-      // Extract the bot's response from the API response
-      const botResponse = response.data.response;
-
-      // Update the state with the bot's response
-      setMessages([...messages, newMessage, botResponse]);
-
-      // Clear the input field after sending the message
-      setNewMessage('');
-    } catch (error) {
-      console.error('Error sending message:', error);
-    }
-  };
-
-  const handleKeyPress = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    // Check if Enter key is pressed (key code 13)
-    if (event.key === 'Enter') {
-      // Prevent default behavior of Enter key (e.g., new line)
-      event.preventDefault();
-      // Send the message
-      sendMessage();
-    }
-  };
-
-  return (
-    <div className="flex h-screen bg-black text-white">
-      <div className="flex flex-col flex-grow justify-between py-10 h-screen bg-black text-white">
-        <div className="flex items-center justify-between px-6">
-          <h1 className="text-3xl font-bold">HealthME</h1>
-          <UserCircleIcon className="h-8 w-8 text-gray-500" />
-        </div>
-        <div className="flex flex-col flex-grow px-6">
-          <div className="flex-grow overflow-y-auto">
-            {/* Display Messages */}
-            {messages.map((message, index) => (
-              <div key={index} className="flex justify-end mb-2">
-                <div className="bg-blue-500 text-white rounded-lg p-2 max-w-xs">{message}</div>
-              </div>
-            ))}
-          </div>
-          <div className="flex items-center">
-            <TextareaAutosize
-              rows={1}
-              placeholder="Message HealthME..."
-              value={newMessage}
-              onChange={(e) => setNewMessage(e.target.value)}
-              onKeyPress={handleKeyPress} // Add key press event listener
-              className="flex-grow bg-gray-800 rounded-lg text-white border-none focus:outline-none p-3 shadow-md text-white-800"
-            />
-            <button onClick={sendMessage} className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg ml-2">Send</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+const getCurrentUser = async () => {
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session?.user?.email) return;
+    const currentUser = await prisma.user.findUnique({
+      where: { email: session.user.email }
+    });
+    if (!currentUser) return;
+    return currentUser;
+  } catch (e: any) {
+    // simply ignores if no user is logged in
+    return;
+  }
 };
 
-export default App;
+export default async function New() {
+  const user = await getCurrentUser();
+  if(!(user)) {
+    return (
+    <>
+        <header>
+          <nav className="border-gray-200 px-4 lg:px-6 py-2.5">
+              <div className="flex flex-nowrap justify-between items-center mx-auto max-w-screen-xl">
+                  <Link href="/" className="flex items-center">
+                      <img src="/logo.png" className="mr-3 h-6 sm:h-9" alt="HealthMe Logo" />
+                      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">HealthMe</span>
+                  </Link>
+                  <div className="flex items-center">
+                    <Link href="/login" className="px-4 py-1.5 mr-2 text-sm font-medium leading-6 text-gray-600 bg-gray-100 rounded-md hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-blue-800">Login</Link>
+                    <Link href="/register" className="px-4 py-1.5 text-sm font-medium leading-6 text-white bg-gradient-to-br from-purple-600 to-blue-500 rounded-md hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-800 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]">Register</Link>
+                  </div>
+              </div>
+            </nav>
+        </header>
+        <main className="flex items-center justify-center px-4 mx-auto max-w-screen-xl min-h-screen text-center lg:px-12">
+          <div className="w-full">
+            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none  md:text-5xl lg:text-6xl text-white">Your Personal <span className="fade-in-text bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">Health Assistant.</span></h1>
+            <p className="mb-8 text-lg font-normal  lg:text-xl sm:px-16 xl:px-48 text-gray-400">AI Expertise, Seamless Health Data Tracking, and Doctor Search</p>
+            <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+                <Link href="/register" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-indigo-600 bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-800 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]">
+                    Get Started
+                    <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </Link>
+            </div>  
+          </div>
+        </main>
+        <footer>
+          <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+            <div className="sm:flex sm:items-center sm:justify-between">
+                <span className="text-sm sm:text-center text-gray-400">© 2024 HealthMe™. All Rights Reserved.
+                </span>
+            </div>
+          </div>
+        </footer>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <header>
+            <nav className="border-gray-200 px-4 lg:px-6 py-2.5">
+              <div className="flex flex-nowrap justify-between items-center mx-auto max-w-screen-xl">
+                  <Link href="/" className="flex items-center">
+                      <img src="/logo.png" className="mr-3 h-6 sm:h-9" alt="HealthMe Logo" />
+                      <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">HealthMe</span>
+                  </Link>
+                  <div className="flex items-center">
+                    <Profile user={user} />
+                  </div>
+              </div>
+            </nav>
+        </header>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+              <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <Link href="/chat" className="block max-w-sm p-6 mt-2 border rounded-lg bg-gray-800 border-gray-700 hover:bg-gray-700 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>New Session</h5>
+                </Link>
+                <Link href="#" className="block max-w-sm p-6 mt-4 border rounded-lg bg-gray-800 border-gray-700 hover:bg-gray-700 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">PlaceholderDate1 Session</h5>
+                </Link>
+                <Link href="#" className="block max-w-sm p-6 mt-4 border rounded-lg bg-gray-800 border-gray-700 hover:bg-gray-700 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">PlaceholderDate2 Session</h5>
+                </Link>
+              </div>
+          </div>
+        </main>
+        <footer>
+          <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+            <div className="sm:flex sm:items-center sm:justify-between">
+                <span className="text-sm sm:text-center text-gray-400">© 2024 HealthMe™. All Rights Reserved.
+                </span>
+            </div>
+          </div>
+        </footer>
+      </>
+    );
+  }
+}
