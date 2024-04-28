da1d3e2a09f05c4c1cab2ddb4475fa95c8489483
SyedTDK <syedfaizullah123@gmail.com>
2024-04-28 00:30:19 -0400

    Update page.tsx

--- src/app/healthAnalytics/vitals/page.tsx
+++ src/app/healthAnalytics/vitals/page.tsx
@@ -8,8 +8,8 @@ import Sidebar, { SidebarItem } from "../../components/Sidebar";
 import { BotMessageSquare, UserSearch, LayoutDashboard, History, Pill, ClipboardList, ShieldBan } from "lucide-react";
 import ConvertToEasternTime from "../../components/ConvertToEasternTime";
 import AddVitals from "@/app/components/AddVitals";
-import AddAllergy from "@/app/components/AddAllergy";
 import Profile from "@/app/components/Profile";
+import DeleteData from "@/app/components/DeleteData";
 
 
 const getCurrentUser = async () => {
@@ -26,16 +26,6 @@ const getCurrentUser = async () => {
       return;
     }
 };
-
-const deleteVital = async (id: number) => {
-    try {
-      await prisma.vitals.delete({
-        where: { id: id }
-      });
-    } catch (error) {
-      console.error('Error deleting vital:', error);
-    }
-}
 
 const getVitals = async (user: any) => {
     try {
@@ -252,9 +242,7 @@ export default async function New() {
                         }
                       </div>
                       
-                      <button onClick={() => deleteVital(vital?.id)} className="flex text-red-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-trash-2"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg>
-                      </button>
+                      <DeleteData dataId={vital.id} type="vital" ></DeleteData>
                     </div>
                   
                 </div>
